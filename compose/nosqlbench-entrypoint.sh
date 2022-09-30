0cd91438fa57ad6410381010c867484e9ccca671
Gianluca Righetto <gianluca@menttis.com>
2022-09-30 10:33:50 -0300

    ZDM-304 NB params cleanup.

--- compose/nosqlbench-entrypoint.sh
+++ compose/nosqlbench-entrypoint.sh
@@ -56,23 +56,21 @@ java -jar /nb.jar \
 echo "Running NoSQLBench VERIFY job on ORIGIN"
 java -jar /nb.jar \
   --show-stacktraces \
-  --report-csv-to /source/verify-origin.csv \
+  --report-csv-to /source/verify-origin \
   /source/nb-tests/cql-nb-activity.yaml \
   verify \
   driver=cqld3 \
   hosts=zdm_tests_origin \
   localdc=datacenter1 \
-  errors=retry \
   -v
 
 echo "Running NoSQLBench VERIFY job on TARGET"
 java -jar /nb.jar \
   --show-stacktraces \
-  --report-csv-to /source/verify-target.csv \
+  --report-csv-to /source/verify-target \
   /source/nb-tests/cql-nb-activity.yaml \
   verify \
   driver=cqld3 \
   hosts=zdm_tests_target \
   localdc=datacenter1 \
-  errors=retry \
   -v
