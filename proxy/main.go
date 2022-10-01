42838a457b917468fa7c7db2fd9490e83782daa5
Bret McGuire <bret.mcguire@datastax.com>
2022-09-30 19:07:40 -0500

    Add (extremely) basic command versioning

--- proxy/main.go
+++ proxy/main.go
@@ -4,6 +4,21 @@
 
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var displayVersion = flag.Bool("version", false, "Display the zdm-proxy version and exit")
+
 func main() {
+
+	flag.Parse()
+	if *displayVersion {
+		fmt.Printf("zdm-proxy version %v\n", "2.0")
+		os.Exit(0)
+	}
+
 	launchProxy(false)
 }
