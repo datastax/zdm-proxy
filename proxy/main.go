21699c764c68e91fcca7193026f1da6f13cb8c86
Bret McGuire <bret.mcguire@datastax.com>
2022-09-30 20:10:12 -0500

    Added explicit logging of version info early in startup process

--- proxy/main.go
+++ proxy/main.go
@@ -8,7 +8,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 )
+
+// TODO: to be managed externally
+const ZdmVersionNumber = "2.0"
 
 var displayVersion = flag.Bool("version", false, "Display the zdm-proxy version and exit")
 
@@ -16,9 +21,12 @@ func main() {
 
 	flag.Parse()
 	if *displayVersion {
-		fmt.Printf("zdm-proxy version %v\n", "2.0")
+		fmt.Printf("zdm-proxy version %v\n", ZdmVersionNumber)
 		os.Exit(0)
 	}
+
+	// Always record version information (very) early in the log
+	log.Infof("zdm-proxy version %v", ZdmVersionNumber)
 
 	launchProxy(false)
 }
