6b0fe268a2eb893dcf3cb2a5d7e31e299a65ac9a
Gianluca Righetto <gianluca@menttis.com>
2023-01-09 13:51:10 -0300

    ZDM-287 Concurrency improvements in stream id test.

--- integration-tests/streamid_test.go
+++ integration-tests/streamid_test.go
@@ -14,6 +14,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"net/http"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -21,7 +22,7 @@ import (
 
 type resources struct {
 	setup  *setup.SimulacronTestSetup
-	client *client.TestClient
+	testClient *client.TestClient
 	wg      *sync.WaitGroup
 	metrics *http.Server
 	close   func()
@@ -31,50 +32,76 @@ func TestStreamIdReads(t *testing.T) {
 	resources := setupResources(t)
 	defer resources.close()
 
-	assertUsedStreamIds := initAsserts(t, resources.setup)
-	asyncQuery := asyncRunner(t, resources.client)
+	assertUsedStreamIds := initAsserts(resources.setup)
+	asyncQuery := asyncContextWrap(resources.testClient)
 
-	t.Run("Test SELECT statements", func(t *testing.T) {
-		query := "SELECT * FROM fake.tbl"
+	t.Run("Test single SELECT statement", func(t *testing.T) {
+		query := fmt.Sprintf("SELECT * FROM fake.%v", strings.ReplaceAll(t.Name(), "/", "_"))
 		primeClustersWithDelay(resources.setup, query)
 
-		wg := asyncQuery(query)
-		time.Sleep(500*time.Millisecond)
+		wg := asyncQuery(t, query, 1)
+		time.Sleep(1*time.Second)
 
-		assertUsedStreamIds(resources.setup.Origin, 1)
-		assertUsedStreamIds(resources.setup.Target, 0)
+		assertUsedStreamIds(t, resources.setup.Origin, 1)
+		assertUsedStreamIds(t, resources.setup.Target, 0)
 		wg.Wait()
-		assertUsedStreamIds(resources.setup.Origin, 0)
-		assertUsedStreamIds(resources.setup.Target, 0)
+		assertUsedStreamIds(t, resources.setup.Origin, 0)
+		assertUsedStreamIds(t, resources.setup.Target, 0)
 	})
 
-	t.Run("Test INSERT statements", func(t *testing.T) {
-		query := "INSERT INTO test (a, b) VALUES (1, 2)"
+	t.Run("Test single INSERT statement", func(t *testing.T) {
+		query := fmt.Sprintf("INSERT INTO fake.%v (a, b) VALUES (1, 2)",  strings.ReplaceAll(t.Name(), "/", "_"))
 		primeClustersWithDelay(resources.setup, query)
 
-		wg := asyncQuery(query)
-		time.Sleep(500*time.Millisecond)
+		wg := asyncQuery(t, query, 1)
+		time.Sleep(1*time.Second)
 
-		assertUsedStreamIds(resources.setup.Origin, 1)
-		assertUsedStreamIds(resources.setup.Target, 1)
+		assertUsedStreamIds(t, resources.setup.Origin, 1)
+		assertUsedStreamIds(t, resources.setup.Target, 1)
 		wg.Wait()
-		assertUsedStreamIds(resources.setup.Origin, 0)
-		assertUsedStreamIds(resources.setup.Target, 0)
+		assertUsedStreamIds(t, resources.setup.Origin, 0)
+		assertUsedStreamIds(t, resources.setup.Target, 0)
+	})
+
+	t.Run("Test multiple INSERT statements", func(t *testing.T) {
+		query := fmt.Sprintf("INSERT INTO fake.%v (a, b) VALUES (1, 2)", strings.ReplaceAll(t.Name(), "/", "_"))
+		primeClustersWithDelay(resources.setup, query)
+
+		repeat := 5
+		wg := asyncQuery(t, query, repeat)
+		time.Sleep(10*time.Second)
+
+		assertUsedStreamIds(t, resources.setup.Origin, repeat)
+		assertUsedStreamIds(t, resources.setup.Target, repeat)
+		wg.Wait()
+		assertUsedStreamIds(t, resources.setup.Origin, 0)
+		assertUsedStreamIds(t, resources.setup.Target, 0)
 	})
 
 }
 
-// asyncRunner is a higher-order function that holds a reference to the test client and returns a function that
+// asyncContextWrap is a higher-order function that holds a reference to the test client and returns a function that
 // actually executes the query in an asynchronous fashion and returns an WaitGroup for synchronization
-func asyncRunner(t *testing.T, client *client.TestClient) func(query string) *sync.WaitGroup {
-	run := func(query string) *sync.WaitGroup {
-		queryWg := &sync.WaitGroup{}
-		queryWg.Add(1)
-		go func() {
-			defer queryWg.Done()
-			executeQuery(t, client, query)
-		}()
-		return queryWg
+func asyncContextWrap(testClient *client.TestClient) func(t *testing.T, query string, repeat int) *sync.WaitGroup {
+	run := func(t *testing.T, query string, repeat int) *sync.WaitGroup {
+		// WaitGroup for controlling the dispatched/sent queries
+		dispatchedWg := &sync.WaitGroup{}
+		// WaitGroup for controlling the return of the response
+		returnedWg := &sync.WaitGroup{}
+		dispatchedWg.Add(repeat)
+		returnedWg.Add(repeat)
+		for i := 0; i < repeat; i++ {
+			log.Infof("repeating: %v", i)
+			go func(testClient *client.TestClient, dispatched *sync.WaitGroup, returned *sync.WaitGroup) {
+				defer returnedWg.Done()
+				dispatchedWg.Done()
+				log.Infof("sending: %v", i)
+				executeQuery(t, testClient, query)
+				log.Infof("sent: %v", i)
+			}(testClient, dispatchedWg, returnedWg)
+		}
+		dispatchedWg.Wait()
+		return returnedWg
 	}
 	return run
 }
@@ -86,11 +113,9 @@ func setupResources(t *testing.T) *resources {
 	simulacronSetup, err := setup.NewSimulacronTestSetup(t)
 	require.Nil(t, err)
 
-	testClient, err := client.NewTestClientWithRequestTimeout(context.Background(), "127.0.0.1:14002", 2 * time.Second)
-	if err != nil {
-		t.Log("Unable to connect to proxy session.")
-		t.Fatal(err)
-	}
+
+	testClient, err := client.NewTestClientWithRequestTimeout(context.Background(), "127.0.0.1:14002", 60 * time.Second)
+	require.Nil(t, err)
 
 	wg := &sync.WaitGroup{}
 	srv := startMetricsHandler(t, simulacronSetup.Proxy.Conf, wg, metricsHandler)
@@ -99,13 +124,11 @@ func setupResources(t *testing.T) *resources {
 
 	return &resources{
 		setup:   simulacronSetup,
-		client:  testClient,
+		testClient:  testClient,
 		wg:      wg,
 		metrics: srv,
 		close: func(){
-			testClient.Shutdown()
 			simulacronSetup.Cleanup()
-			testClient.Shutdown()
 			err := srv.Close()
 			if err != nil {
 				log.Warnf("error cleaning metrics server: %v", err)
@@ -122,8 +145,7 @@ func primeClustersWithDelay(setup *setup.SimulacronTestSetup, query string) {
 		simulacron.WhenQuery(
 			query,
 			simulacron.NewWhenQueryOptions()).
-			ThenSuccess().WithDelay(1 * time.Second)
-
+			ThenSuccess().WithDelay(3 * time.Second)
 
 	setup.Origin.Prime(queryPrimeDelayedResponse)
 	setup.Target.Prime(queryPrimeDelayedResponse)
@@ -132,6 +154,7 @@ func primeClustersWithDelay(setup *setup.SimulacronTestSetup, query string) {
 // executeQuery sends the query string in a Frame message through the test client and handles any failures internally
 // by failing the tests, otherwise, returns the response to the caller
 func executeQuery(t *testing.T, client *client.TestClient, query string) *frame.Frame {
+	log.Info("executing query")
 	q := &message.Query{
 		Query: query,
 	}
@@ -144,8 +167,8 @@ func executeQuery(t *testing.T, client *client.TestClient, query string) *frame.
 
 // initAsserts is a higher-order function that holds a reference to the setup structs which
 // allows the inner function to take only the necessary assertion params
-func initAsserts(t *testing.T, setup *setup.SimulacronTestSetup) func (cluster *simulacron.Cluster, expectedValue int) {
-	asserts := func (cluster *simulacron.Cluster, expectedValue int) {
+func initAsserts(setup *setup.SimulacronTestSetup) func (t *testing.T, cluster *simulacron.Cluster, expectedValue int) {
+	asserts := func (t *testing.T, cluster *simulacron.Cluster, expectedValue int) {
 		lines := GatherMetrics(t, setup.Proxy.Conf, true)
 		prefix := "zdm"
 		endpoint := fmt.Sprintf("%v:9042", cluster.InitialContactPoint)
