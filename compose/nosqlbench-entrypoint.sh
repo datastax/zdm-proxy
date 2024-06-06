6807d499f278acc8bc9420c262dc3cee18b26cda
Madhavan Sridharan <madhavan.sridharan@datastax.com>
2024-06-06 09:54:01 -0400

    Update NoSQLBench to 5.21-latest & other minor updates

--- compose/nosqlbench-entrypoint.sh
+++ compose/nosqlbench-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-apk add --no-cache netcat-openbsd
-apk add py3-pip
+
+apt -qq update -y
+apt -qq install netcat-openbsd python3-pip -y
+
+echo "deb https://downloads.datastax.com/deb stable main" | tee -a /etc/apt/sources.list.d/datastax.sources.list
+curl -sL https://downloads.datastax.com/deb/doc/apt_key.gpg | apt-key add -
 pip install cqlsh
 
-function test_conn() {
+function test_conn {
 	nc -z -v  $1 9042;
 	while [ $? -ne 0 ];
 		do echo "CQL port not ready on $1";
@@ -17,62 +21,57 @@ test_conn zdm_tests_origin
 test_conn zdm_tests_target
 test_conn zdm_tests_proxy
 
-set -e
+set -xe
 
 echo "Creating schema"
 cat /source/nb-tests/schema.cql | cqlsh zdm_tests_proxy
 
 echo "Running NoSQLBench RAMPUP job"
-java -jar /nb.jar \
+java -jar /nb5.jar \
   --show-stacktraces \
   /source/nb-tests/cql-nb-activity.yaml \
   rampup \
-  driver=cqld3 \
   hosts=zdm_tests_proxy \
   localdc=datacenter1 \
-  errors=retry \
+  errors=counter,retry \
   -v
 
 echo "Running NoSQLBench WRITE job"
-java -jar /nb.jar \
+java -jar /nb5.jar \
   --show-stacktraces \
   /source/nb-tests/cql-nb-activity.yaml \
   write \
-  driver=cqld3 \
   hosts=zdm_tests_proxy \
   localdc=datacenter1 \
-  errors=retry \
+  errors=counter,retry \
   -v
 
 echo "Running NoSQLBench READ job"
-java -jar /nb.jar \
+java -jar /nb5.jar \
   --show-stacktraces \
   /source/nb-tests/cql-nb-activity.yaml \
   read \
-  driver=cqld3 \
   hosts=zdm_tests_proxy \
   localdc=datacenter1 \
-  errors=retry \
+  errors=counter,retry \
   -v
 
 echo "Running NoSQLBench VERIFY job on ORIGIN"
-java -jar /nb.jar \
+java -jar /nb5.jar \
   --show-stacktraces \
   --report-csv-to /source/verify-origin \
   /source/nb-tests/cql-nb-activity.yaml \
   verify \
-  driver=cqld3 \
   hosts=zdm_tests_origin \
   localdc=datacenter1 \
   -v
 
 echo "Running NoSQLBench VERIFY job on TARGET"
-java -jar /nb.jar \
+java -jar /nb5.jar \
   --show-stacktraces \
   --report-csv-to /source/verify-target \
   /source/nb-tests/cql-nb-activity.yaml \
   verify \
-  driver=cqld3 \
   hosts=zdm_tests_target \
   localdc=datacenter1 \
   -v
