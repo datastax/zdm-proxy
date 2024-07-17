1dabb2385d26a831114b0f89ea10f5f80305ac66
Lukasz Antoniak <Lukasz.Antoniak@gmail.com>
2024-07-17 16:03:23 +0200

    Cleanup

--- proxy/pkg/zdmproxy/clienthandler_test.go
+++ proxy/pkg/zdmproxy/clienthandler_test.go
@@ -22,8 +22,8 @@ func TestMaxStreamIds(t *testing.T) {
 		{"OriginV3_TargetV3_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion3, targetProtoVer: primitive.ProtocolVersion3, config: &config.Config{ProxyMaxStreamIds: 2048}}, 2048},
 		{"OriginV3_TargetV4_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion3, targetProtoVer: primitive.ProtocolVersion4, config: &config.Config{ProxyMaxStreamIds: 2048}}, 2048},
 		{"OriginV3_TargetV4_LowerConfig", args{originProtoVer: primitive.ProtocolVersion3, targetProtoVer: primitive.ProtocolVersion4, config: &config.Config{ProxyMaxStreamIds: 1024}}, 1024},
-		{"OriginV2_TargetV3_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion2, targetProtoVer: primitive.ProtocolVersion3, config: &config.Config{ProxyMaxStreamIds: 2048}}, 128},
-		{"OriginV2_TargetV2_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion2, targetProtoVer: primitive.ProtocolVersion2, config: &config.Config{ProxyMaxStreamIds: 2048}}, 128},
+		{"OriginV2_TargetV3_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion2, targetProtoVer: primitive.ProtocolVersion3, config: &config.Config{ProxyMaxStreamIds: 2048}}, 127},
+		{"OriginV2_TargetV2_DefaultConfig", args{originProtoVer: primitive.ProtocolVersion2, targetProtoVer: primitive.ProtocolVersion2, config: &config.Config{ProxyMaxStreamIds: 2048}}, 127},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
